48b7050ba54c3889db13b7102da0216ebf7989ae
Nilam-Vaidya <nilamvaidya100@gmail.com>
2024-02-06 19:23:35 +0530

    cross btn and div issue solved

--- scripts/bloglist.js
+++ scripts/bloglist.js
@@ -1,31 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
+// document.addEventListener("DOMContentLoaded", function() {
 
-    var modal = document.getElementById("createpostbtnmodalid");
-    // var btn =document.getElementById("createpostbtn");
-    var span =document.getElementsByClassName("close")[0];
+//     var modal = document.getElementById("createpostbtnmodalid");
+//     var btn =document.getElementById("createpostbtn");
+//     var span =document.getElementsByClassName("close")[0];
         
-    // btn.onclick=function(){
-    //     modal.style.display="block";
-    // }
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-    // Open Create Post Modal
-    document.getElementById('createpostbtnmodalid').addEventListener('click', function() {
-        document.getElementById('createpostbtnmodalid').style.display = 'block';
-    });
+//     btn.onclick=function(){
+//         modal.style.display="block";
+//     }
+//     span.onclick = function() {
+//         modal.style.display = "none";
+//     }
+//     // Open Create Post Modal
+//     document.getElementById('createpostbtnmodalid').addEventListener('click', function() {
+//         document.getElementById('createpostbtnmodalid').style.display = 'block';
+//     });
 
-    // Close Create Post Modal
-    document.getElementById('closebutton').addEventListener('click', function() {
-        document.getElementById('closebutton').style.display = "none";
-    });
+//     // Close Create Post Modal
+//     document.getElementById('closebutton').addEventListener('click', function() {
+//         document.getElementById('closebutton').style.display = "none";
+//     });
 
-});
+// });
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    setupModal("createpostbtnmodalid", "closebutton", "closebutton","closebutton");
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     setupModal("createpostbtnmodalid", "closebutton", "closebutton","closebutton");
+// });
 
 // function setupModal(modalId, btnId, closeBtnId, createBtnId) {
 //     var modal = document.getElementById(modalId);
@@ -56,16 +56,22 @@ document.addEventListener("DOMContentLoaded", function() {
 //     document.getElementById("createpostbtnmodalid").style.display='none';
 // });
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     var modal = document.getElementById("createpostbtnmodalid");
-//     var btn =document.getElementById("createPostButtonModal");
-//     var span =document.getElementById("close")[1];
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("createpostbtnmodalid");
+    var btn =document.getElementById("createPostButtonModal");
+    var span =document.getElementById("closebutton");
 
-//     btn.onclick=function(){
-//         modal.style.display="block";
-//     }
-//     span.onclick = function() {
-//         modal.style.display = "none";
-//     }
-// });
+    btn.onclick=function(){
+        modal.style.display="block";
+    }
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
 
